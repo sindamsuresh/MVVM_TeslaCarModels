6b7b9c78d6e6a126d7a6bbcd670adacc7c5ad5c2
SureshSindam <sindam.suresh@gmail.com>
2023-12-24 16:43:51 -0600

    MVVM folder setup

---
+++ MVVM_TeslaCarModels/Controller/CarModelsVC.swift
@@ -0,0 +1,29 @@
+//
+//  CarModelsVC.swift
+//  MVVM_TeslaCarModels
+//
+//  Created by Suresh Sindam on 12/24/23.
+//
+
+import UIKit
+
+class CarModelsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
